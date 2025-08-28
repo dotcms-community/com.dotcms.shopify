eef91169d0892db27751d07d596b5b4d69a4c532
Will Ezell <will@dotcms.com>
2025-08-28 14:03:14 -0400

    collection picker working

--- src/main/java/com/dotcms/shopify/rest/ShopifyCollectionResource.java
+++ src/main/java/com/dotcms/shopify/rest/ShopifyCollectionResource.java
@@ -6,8 +6,11 @@ import com.dotcms.shopify.api.ProductSearchParams;
 import com.dotcms.shopify.api.ShopifyAPI;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.web.WebAPILocator;
+import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.util.UtilMethods;
 import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.BeanParam;
@@ -21,61 +24,83 @@ import javax.ws.rs.core.Response;
 @Path("/v1/shopify/collection")
 public class ShopifyCollectionResource implements Serializable {
 
-  private static final long serialVersionUID = 204840922704940654L;
+    private static final long serialVersionUID = 204840922704940654L;
 
-  @GET
-  @Path("/")
-  @NoCache
-  @Produces(MediaType.APPLICATION_JSON)
-  public final Response byId(
-      @Context final HttpServletRequest request,
-      @Context final HttpServletResponse response,
-      @BeanParam final ProductSearchParams searchParams) {
+    @GET
+    @Path("/")
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    public final Response byId(
+            @Context final HttpServletRequest request,
+            @Context final HttpServletResponse response,
+            @BeanParam final ProductSearchParams searchParams) {
 
-    checkUser(request, response);
+        checkUser(request, response);
 
-    if (UtilMethods.isEmpty(searchParams.id)) {
-      return Response.status(404).build();
+        if (UtilMethods.isEmpty(searchParams.id)) {
+            return Response.status(404).build();
+        }
+
+        Host host = WebAPILocator.getHostWebAPI().getCurrentHostNoThrow(request);
+
+        ShopifyAPI api = ShopifyAPI.api(host);
+
+        return Response.ok(api.collectionById(searchParams.id)).build();
     }
 
-    Host host = WebAPILocator.getHostWebAPI().getCurrentHostNoThrow(request);
+    @GET
+    @Path("/_search")
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    public final Response searchCollections(
+            @Context final HttpServletRequest request,
+            @Context final HttpServletResponse response,
+            @BeanParam final ProductSearchParams searchParams) {
 
-    ShopifyAPI api = ShopifyAPI.api(host);
+        checkUser(request, response);
 
-    return Response.ok(api.collectionById(searchParams.id)).build();
-  }
+        if (UtilMethods.isEmpty(searchParams.query)) {
+            return Response.status(500, "Invalid argument").build();
+        }
 
-  @GET
-  @Path("/_search")
-  @NoCache
-  @Produces(MediaType.APPLICATION_JSON)
-  public final Response searchCollections(
-      @Context final HttpServletRequest request,
-      @Context final HttpServletResponse response,
-      @BeanParam final ProductSearchParams searchParams) {
+        Host host = WebAPILocator.getHostWebAPI().getCurrentHostNoThrow(request);
+        ShopifyAPI api = ShopifyAPI.api(host);
+        return Response
+                .ok(api.searchCollections(searchParams.toProductSearcher()))
+                .build();
 
-    checkUser(request, response);
-
-    if (UtilMethods.isEmpty(searchParams.query)) {
-      return Response.status(500, "Invalid argument").build();
     }
 
-    Host host = WebAPILocator.getHostWebAPI().getCurrentHostNoThrow(request);
-    ShopifyAPI api = ShopifyAPI.api(host);
-    return Response
-        .ok(api.searchCollections(searchParams.toProductSearcher()))
-        .build();
+    @GET
+    @Path("/_redirect")
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    public final Response redirectToShopifyCollection(@Context final HttpServletRequest request,
+            @Context final HttpServletResponse response, @BeanParam final ProductSearchParams searchParams)
+            throws URISyntaxException {
 
-  }
+        checkUser(request, response);
 
-  private void checkUser(final HttpServletRequest request, final HttpServletResponse response) {
-    new WebResource.InitBuilder(request, response)
-        .rejectWhenNoUser(true)
-        .requiredFrontendUser(true)
-        .requiredBackendUser(true)
-        .init()
-        .getUser();
+        if (UtilMethods.isEmpty(searchParams.id)) {
+            throw new DotRuntimeException("missing collection Id");
+        }
 
-  }
+        Host host = WebAPILocator.getHostWebAPI().getCurrentHostNoThrow(request);
+
+        String redirectUrl = ShopifyAPI.api(host).linkToShopifyCollection(searchParams.id);
+
+        return Response.status(Response.Status.FOUND).location(new URI(redirectUrl)).build();
+
+    }
+
+    private void checkUser(final HttpServletRequest request, final HttpServletResponse response) {
+        new WebResource.InitBuilder(request, response)
+                .rejectWhenNoUser(true)
+                .requiredFrontendUser(true)
+                .requiredBackendUser(true)
+                .init()
+                .getUser();
+
+    }
 
 }
